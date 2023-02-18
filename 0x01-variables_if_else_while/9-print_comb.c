82497d948b8d6ef06b6b7e1837ed5ba21677f1f3
AndersonOkeke <okekeifeanyia@gmail.com>
2023-02-17 17:02:27 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - Print Numbers.
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar('0' + a);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
