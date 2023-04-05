5a0dbfbfaffea9d1ab85a254538918ed298bc36f
AndersonOkeke <okekeifeanyia@gmail.com>
2023-04-04 18:55:39 -0700

    update

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -14,8 +14,8 @@
  */
 typedef struct listint_s
 {
- int n;
- struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -34,5 +34,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 int _putchar(char c);
 listint_t *find_listint_loop(listint_t *head);
-
 #endif
