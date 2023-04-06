3a6b71e5afe88649392e40fa6d950f09e86f64ec
AndersonOkeke <okekeifeanyia@gmail.com>
2023-04-06 04:45:22 -0700

    Task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,8 +3,8 @@
 /**
  * binary_to_uint - converts a binary number to unsigned int
  * @b: string containing the binary number
- * Return: 0 if b is NULL
- * Return: 0 if b is not 0 or 1
+ * 0 if b is NULL
+ * 0 if b is not 0 or 1
  * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
